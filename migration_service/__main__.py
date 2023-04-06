34e2827344e07f83d5dcda1df93c29644d4aab59
a.gudimov <anton.gudimov@itentika.ru>
2023-04-06 13:46:52 +0300

    fixed bug with uvicorn restarting

--- migration_service/__main__.py
+++ migration_service/__main__.py
@@ -9,10 +9,9 @@ config_logger()
 
 
 def main() -> None:
-    from migration_service.app import migration_app
-    app = migration_app
     port = int(os.environ.get("PORT", 8081))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    reload = bool(os.environ.get("RELOAD", True))
+    uvicorn.run("migration_service.app:migration_app", host="0.0.0.0", port=port, reload=reload)
 
 
 if __name__ == "__main__":
