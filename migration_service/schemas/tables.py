084998b23303b383071cd710486b9394053d5ecb
a.gudimov <anton.gudimov@itentika.ru>
2023-04-06 12:59:10 +0300

    minor renaming

--- migration_service/schemas/tables.py
+++ migration_service/schemas/tables.py
@@ -5,7 +5,7 @@ from typing import Dict
 @dataclass(frozen=True)
 class Table:
     name: str
-    fields: Dict[str, str] = field(default_factory=dict)
+    field_to_type: Dict[str, str] = field(default_factory=dict)
 
     def __hash__(self):
-        return hash((self.name, self.fields))
+        return hash((self.name, self.field_to_type))
