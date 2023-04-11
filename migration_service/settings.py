03d1440ebea83760909eff0a2cf99f2f5cf60ecf
elijah <cuiyeqing960904@gmail.com>
2023-04-11 08:27:32 -0400

    updated env

--- migration_service/settings.py
+++ migration_service/settings.py
@@ -2,6 +2,8 @@ from collections import namedtuple
 
 from pydantic import BaseSettings
 
+import os
+
 Neo4jCreds = namedtuple('Neo4jCreds', ['username', 'password'])
 
 
@@ -15,16 +17,16 @@ class Settings(BaseSettings):
     log_name: str = "graph_db_migrater.log"
 
     db_sources: dict = {
-        'dv_raw': 'postgresql://postgres:dwh@db.lan:5432/dwh'
+        'dv_raw': os.environ.get('DWH_GRAPH_DB_SOURCE', 'postgresql://postgres:dwh@db.lan:5432/dwh')
     }
 
     # Database constants
-    db_connection_string: str = 'postgresql+asyncpg://postgres:dwh@db.lan:5432/graph_migrations'
-    db_migration_connection_string: str = 'postgresql+psycopg2://postgres:dwh@db.lan:5432/graph_migrations'
+    db_connection_string: str = os.environ.get('DWH_GRAPH_DB_CONNECTION_STRING', 'postgresql+asyncpg://postgres:dwh@db.lan:5432/graph_migrations')
+    db_migration_connection_string: str = os.environ('DWH_GRAPH_DB_MIGRATION_STRING', 'postgresql+psycopg2://postgres:dwh@db.lan:5432/graph_migrations')
 
     # Neo4j constants
-    neo4j_connection_string: str = 'bolt://graphdb.lan:7687'
-    neo4j_auth: Neo4jCreds = ('neo4j', 'dwh')
+    neo4j_connection_string: str = os.environ.get('DWH_GRAPH_DB_NEO4J_CONNECTION_STRING', 'bolt://graphdb.lan:7687')
+    neo4j_auth: Neo4jCreds = (os.environ.get('DWH_GRAPH_DB_NEO4J_CONNECTION_USER', 'neo4j'), os.environ.get('DWH_GRAPH_DB_NEO4J_CONNECTION_PASSWORD', 'dwh'))
 
     # Service's urls
     api_iam = 'http://iam.lan:8000'
