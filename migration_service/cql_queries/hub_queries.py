f1b93222adb3d71b61d3ca8f69039b301f1cb459
a.gudimov <anton.gudimov@itentika.ru>
2023-04-14 16:00:46 +0300

    fixed bug with field db name

--- migration_service/cql_queries/hub_queries.py
+++ migration_service/cql_queries/hub_queries.py
@@ -3,5 +3,5 @@ create_hubs_query = "WITH $hubs as hub_batch " \
                     "CREATE (hub:Entity {uuid: randomUUID(), name: hub_record.name, db: $db_source + '.' + hub_record.name}) " \
                     "WITH hub_record.fields as fields_batch, hub " \
                     "UNWIND fields_batch as field " \
-                    "CREATE (hub)-[:ATTR]->(:Field {name: field.name, db: $db_source + '.' + field.name, attrs: [], dbtype: field.db_type}) " \
+                    "CREATE (hub)-[:ATTR]->(:Field {name: field.name, db: $db_source + '.' + hub.name + '.' + field.name, attrs: [], dbtype: field.db_type}) " \
                     "RETURN hub.uuid as uuid;"
--- migration_service/utils/neo4j_utils.py
+++ migration_service/utils/neo4j_utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from typing import Set
 
 from neo4j import AsyncSession as Neo4jAsyncSession
@@ -17,10 +19,10 @@ async def get_neo4j_tables(graph_session: Neo4jAsyncSession, db_source: str) -> 
 
 async def get_neo4j_table_col_type(table_names: Set[str], db_source: str, graph_session: Neo4jAsyncSession):
     res = await graph_session.run(
-        "MATCH (obj)-[:ATTR]->(f:Field)  "
+        "MATCH (obj)-[:ATTR]->(f:Field) "
         "WITH split(obj.db, $db_source + '.')[1] AS db_name, obj, f "
         "WHERE (obj:Entity OR obj:Sat OR obj:Link) and obj.db STARTS WITH $db_source AND db_name IN $tables "
-        "RETURN db_name as name, f.db as f_name, f.dbtype as f_type "
+        "RETURN db_name as name, split(f.db, $db_source + '.' + db_name + '.')[1] as f_name, f.dbtype as f_type "
         "ORDER BY db_name;",
         db_source=db_source, tables=list(table_names)
     )
